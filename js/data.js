084f2b89322e814c2e61a0a03648fad3c4fa7bae
Ben <ben@wainei.net>
2018-04-08 12:25:10 +0200

    tabs wowee

--- js/data.js
+++ js/data.js
@@ -1,279 +1,279 @@
 let products = [{
-	'name': 'G.Skill Trident Z',
-	'code': 'TZ',
-	'size': [
-		'16G',
-		'32G'
-	],
-	'speed': [
-		'3000C14',
-		'3200C16',
-		'3200C15',
-		'3200C14',
-		'3466C16',
-		'3600C17',
-		'3600C16',
-		'3600C15',
-		'3733C17',
-		'3866C18',
-		'4000C19',
-		'4133C19',
-		'4266C19'
-	],
-	'color': [
-		'TZ',
-		'TZA',
-		'TZR',
-		'TZKO',
-		'TZKW',
-		'TZKY',
-		'TZSK',
-		'TZSW',
-	],
-	'sku': [
-		'F4-3000C14D-16GTZ',
-		'F4-3000C14D-16GTZR',
-		
-		'F4-3000C14D-32GTZ',
-		'F4-3000C14D-32GTZR',
-		
-		'F4-3200C16D-32GTZA',
-		
-		'F4-3200C15D-16GTZ',
-		'F4-3200C15D-16GTZKO',
-		'F4-3200C15D-16GTZKW',
-		'F4-3200C15D-16GTZKY',
-		'F4-3200C15D-16GTZSK',
-		'F4-3200C15D-16GTZSW',
-		
-		'F4-3200C15D-32GTZ',
-		'F4-3200C15D-32GTZKO',
-		'F4-3200C15D-32GTZKW',
-		'F4-3200C15D-32GTZKY',
-		'F4-3200C15D-32GTZR',
-		'F4-3200C15D-32GTZSK',
-		'F4-3200C15D-32GTZSW',
-				
-		'F4-3200C14D-16GTZ',
-		'F4-3200C14D-16GTZKO',
-		'F4-3200C14D-16GTZKW',
-		'F4-3200C14D-16GTZKY',
-		'F4-3200C14D-16GTZR',
-		'F4-3200C14D-16GTZRX',
-		'F4-3200C14D-16GTZSK',
-		'F4-3200C14D-16GTZSW',
-		
-		'F4-3200C14D-32GTZ',
-		'F4-3200C14D-32GTZKO',
-		'F4-3200C14D-32GTZKW',
-		'F4-3200C14D-32GTZKY',
-		'F4-3200C14D-32GTZR',
-		'F4-3200C14D-32GTZSK',
-		'F4-3200C14D-32GTZSW',
-		
-		'F4-3400C16D-32GTZ',
-		
-		'F4-3466C16D-16GTZ',
-		'F4-3466C16D-16GTZKW',
-		'F4-3466C16D-16GTZR',
-		'F4-3466C16D-16GTZSK',
-		'F4-3466C16D-16GTZSW',
-		
-		'F4-3600C17D-16GTZ',
-		'F4-3600C17D-16GTZKW',
-		'F4-3600C17D-16GTZR',
-		'F4-3600C17D-16GTZSW',
-		
-		'F4-3600C16D-16GTZ',
-		'F4-3600C16D-16GTZKW',
-		'F4-3600C16D-16GTZR',
-		'F4-3600C16D-16GTZSW',
-		
-		'F4-3600C15D-16GTZ',
-		
-		'F4-3733C17D-16GTZA',
-		'F4-3733C17D-16GTZKW',
-		'F4-3733C17D-16GTZSW',
-		
-		'F4-3866C18D-16GTZ',
-		'F4-3866C18D-16GTZKW',
-		'F4-3866C18D-16GTZR',
-		'F4-3866C18D-16GTZSW',
-		
-		'F4-4000C19D-16GTZ',
-		'F4-4000C19D-16GTZKW',
-		'F4-4000C19D-16GTZSW',
-		
-		'F4-4133C19D-16GTZA',
-		
-		'F4-4266C19D-16GTZ'
-	]
+    'name': 'G.Skill Trident Z',
+    'code': 'TZ',
+    'size': [
+        '16G',
+        '32G'
+    ],
+    'speed': [
+        '3000C14',
+        '3200C16',
+        '3200C15',
+        '3200C14',
+        '3466C16',
+        '3600C17',
+        '3600C16',
+        '3600C15',
+        '3733C17',
+        '3866C18',
+        '4000C19',
+        '4133C19',
+        '4266C19'
+    ],
+    'color': [
+        'TZ',
+        'TZA',
+        'TZR',
+        'TZKO',
+        'TZKW',
+        'TZKY',
+        'TZSK',
+        'TZSW',
+    ],
+    'sku': [
+        'F4-3000C14D-16GTZ',
+        'F4-3000C14D-16GTZR',
+
+        'F4-3000C14D-32GTZ',
+        'F4-3000C14D-32GTZR',
+
+        'F4-3200C16D-32GTZA',
+
+        'F4-3200C15D-16GTZ',
+        'F4-3200C15D-16GTZKO',
+        'F4-3200C15D-16GTZKW',
+        'F4-3200C15D-16GTZKY',
+        'F4-3200C15D-16GTZSK',
+        'F4-3200C15D-16GTZSW',
+
+        'F4-3200C15D-32GTZ',
+        'F4-3200C15D-32GTZKO',
+        'F4-3200C15D-32GTZKW',
+        'F4-3200C15D-32GTZKY',
+        'F4-3200C15D-32GTZR',
+        'F4-3200C15D-32GTZSK',
+        'F4-3200C15D-32GTZSW',
+
+        'F4-3200C14D-16GTZ',
+        'F4-3200C14D-16GTZKO',
+        'F4-3200C14D-16GTZKW',
+        'F4-3200C14D-16GTZKY',
+        'F4-3200C14D-16GTZR',
+        'F4-3200C14D-16GTZRX',
+        'F4-3200C14D-16GTZSK',
+        'F4-3200C14D-16GTZSW',
+
+        'F4-3200C14D-32GTZ',
+        'F4-3200C14D-32GTZKO',
+        'F4-3200C14D-32GTZKW',
+        'F4-3200C14D-32GTZKY',
+        'F4-3200C14D-32GTZR',
+        'F4-3200C14D-32GTZSK',
+        'F4-3200C14D-32GTZSW',
+
+        'F4-3400C16D-32GTZ',
+
+        'F4-3466C16D-16GTZ',
+        'F4-3466C16D-16GTZKW',
+        'F4-3466C16D-16GTZR',
+        'F4-3466C16D-16GTZSK',
+        'F4-3466C16D-16GTZSW',
+
+        'F4-3600C17D-16GTZ',
+        'F4-3600C17D-16GTZKW',
+        'F4-3600C17D-16GTZR',
+        'F4-3600C17D-16GTZSW',
+
+        'F4-3600C16D-16GTZ',
+        'F4-3600C16D-16GTZKW',
+        'F4-3600C16D-16GTZR',
+        'F4-3600C16D-16GTZSW',
+
+        'F4-3600C15D-16GTZ',
+
+        'F4-3733C17D-16GTZA',
+        'F4-3733C17D-16GTZKW',
+        'F4-3733C17D-16GTZSW',
+
+        'F4-3866C18D-16GTZ',
+        'F4-3866C18D-16GTZKW',
+        'F4-3866C18D-16GTZR',
+        'F4-3866C18D-16GTZSW',
+
+        'F4-4000C19D-16GTZ',
+        'F4-4000C19D-16GTZKW',
+        'F4-4000C19D-16GTZSW',
+
+        'F4-4133C19D-16GTZA',
+
+        'F4-4266C19D-16GTZ'
+    ]
 }, {
-	'name': 'G.Skill Ripjaws V',
-	'code': 'GV',
-	'size': [
-		'16G',
-		'32G',
-	],
-	'speed': [
-		'2800C14',
-		'3000C14',
-		'3000C14',
-		'3200C15',
-		'3200C14',
-		'3466C16',
-		'3600C17',
-		'3600C16'
-	],
-	'color': [
-		'VR',
-		'VK'
-	],
-	'sku': [
-		'F4-2800C14D-32GVK',
-				
-		'F4-3000C14D-16GVK',
-		'F4-3000C14D-16GVR',
-		
-		'F4-3000C14D-32GVK',
-		'F4-3000C14D-32GVR',
-		
-		'F4-3200C15D-16GVK',
-		'F4-3200C15D-16GVR',
-		
-		'F4-3200C15D-32GVK',
-		'F4-3200C15D-32GVR',
-		
-		'F4-3200C14D-16GVK',
-		'F4-3200C14D-16GVR',
-		
-		'F4-3200C14D-32GVK',
-		'F4-3200C14D-32GVR',
-		
-		'F4-3400C16D-32GVK',
-		'F4-3400C16D-32GVR',
-		
-		'F4-3466C16D-16GVK',
-		'F4-3466C16D-16GVR',
-		
-		'F4-3600C17D-16GVK',
-		
-		'F4-3600C16D-16GVK'
-	]
+    'name': 'G.Skill Ripjaws V',
+    'code': 'GV',
+    'size': [
+        '16G',
+        '32G',
+    ],
+    'speed': [
+        '2800C14',
+        '3000C14',
+        '3000C14',
+        '3200C15',
+        '3200C14',
+        '3466C16',
+        '3600C17',
+        '3600C16'
+    ],
+    'color': [
+        'VR',
+        'VK'
+    ],
+    'sku': [
+        'F4-2800C14D-32GVK',
+
+        'F4-3000C14D-16GVK',
+        'F4-3000C14D-16GVR',
+
+        'F4-3000C14D-32GVK',
+        'F4-3000C14D-32GVR',
+
+        'F4-3200C15D-16GVK',
+        'F4-3200C15D-16GVR',
+
+        'F4-3200C15D-32GVK',
+        'F4-3200C15D-32GVR',
+
+        'F4-3200C14D-16GVK',
+        'F4-3200C14D-16GVR',
+
+        'F4-3200C14D-32GVK',
+        'F4-3200C14D-32GVR',
+
+        'F4-3400C16D-32GVK',
+        'F4-3400C16D-32GVR',
+
+        'F4-3466C16D-16GVK',
+        'F4-3466C16D-16GVR',
+
+        'F4-3600C17D-16GVK',
+
+        'F4-3600C16D-16GVK'
+    ]
 }, {
-	'name': 'G.Skill Flare X',
-	'code': 'FX',
-	'size': [
-		'16G'
-	],
-	'speed': [
-		'3200C14',
-	],
-	'color': [
-		'FX'
-	],
-	'sku': [
-		'F4-3200C14D-16GFX'
-	]
+    'name': 'G.Skill Flare X',
+    'code': 'FX',
+    'size': [
+        '16G'
+    ],
+    'speed': [
+        '3200C14',
+    ],
+    'color': [
+        'FX'
+    ],
+    'sku': [
+        'F4-3200C14D-16GFX'
+    ]
 }, {
-	'name': 'Corsair Dominator',
-	'code': 'CMD',
-	'size': [
-		'16G',
-		'32G'
-	],
-	'speed': [
-		'3000C15',
-		'3600C16',
-	],
-	'color': [],
-	'sku': [
-		'CMD16GX4M2B3000C15',
-		
-		'CMD32GX4M4B3600C16'
-	]
+    'name': 'Corsair Dominator',
+    'code': 'CMD',
+    'size': [
+        '16G',
+        '32G'
+    ],
+    'speed': [
+        '3000C15',
+        '3600C16',
+    ],
+    'color': [],
+    'sku': [
+        'CMD16GX4M2B3000C15',
+
+        'CMD32GX4M4B3600C16'
+    ]
 }, {
-	'name': 'Corsair Vengeance',
-	'code': 'CMK',
-	'size': [
-		'32G'
-	],
-	'speed': [
-		'3400C16',
-		'3466C16',
-		'3600C16',
-		'3600C18',
-		'4000C19'
-	],
-	'color': [],
-	'sku': [
-		'CMU32GX4M4B3400C16', // FIXME: missing when filtering by model
-		
-		'CMK32GX4M4B3466C16',
-		
-		'CMK32GX4M4B3600C16',
-		
-		'CMK32GX4M4B3600C18',
-		
-		'CMK32GX4M4B4000C19'
-	]
+    'name': 'Corsair Vengeance',
+    'code': 'CMK',
+    'size': [
+        '32G'
+    ],
+    'speed': [
+        '3400C16',
+        '3466C16',
+        '3600C16',
+        '3600C18',
+        '4000C19'
+    ],
+    'color': [],
+    'sku': [
+        'CMU32GX4M4B3400C16', // FIXME: missing when filtering by model
+
+        'CMK32GX4M4B3466C16',
+
+        'CMK32GX4M4B3600C16',
+
+        'CMK32GX4M4B3600C18',
+
+        'CMK32GX4M4B4000C19'
+    ]
 }];
 
 let skus = products.map(product => product.sku);
 skus = skus.reduce((acc, val) => acc.concat(val), []);
 
 let sites = [{
-	'country': 'us',
-	'name': 'Amazon',
-	'url': 'https://www.amazon.com/s/field-keywords=%s'
+    'country': 'us',
+    'name': 'Amazon',
+    'url': 'https://www.amazon.com/s/field-keywords=%s'
 }, {
-	'country': 'us',
-	'name': 'Newegg US',
-	'url': 'https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Description=%s'
+    'country': 'us',
+    'name': 'Newegg US',
+    'url': 'https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Description=%s'
 }, {
-	'country': 'ca',
-	'name': 'Amazon',
-	'url': 'https://www.amazon.ca/field-keywords=%s'
+    'country': 'ca',
+    'name': 'Amazon',
+    'url': 'https://www.amazon.ca/field-keywords=%s'
 }, {
-	'country': 'ca',
-	'name': 'Newegg Canada',
-	'url': 'https://www.newegg.ca/Product/ProductList.aspx?Submit=ENE&DEPA=0&Description=%s'
+    'country': 'ca',
+    'name': 'Newegg Canada',
+    'url': 'https://www.newegg.ca/Product/ProductList.aspx?Submit=ENE&DEPA=0&Description=%s'
 }, {
-	'country': 'ca',
-	'name': 'NCIX',
-	'url': 'https://www.ncix.com/search/?q=%s'
+    'country': 'ca',
+    'name': 'NCIX',
+    'url': 'https://www.ncix.com/search/?q=%s'
 }, {
-	'country': 'de',
-	'name': 'Amazon Germany',
-	'url': 'https://www.amazon.de/s/field-keywords=%s'
+    'country': 'de',
+    'name': 'Amazon Germany',
+    'url': 'https://www.amazon.de/s/field-keywords=%s'
 }, {
-	'country': 'de',
-	'name': 'Idealo Germany',
-	'url': 'https://www.idealo.de/preisvergleich/ProductCategory/26831.html?q=%s'
+    'country': 'de',
+    'name': 'Idealo Germany',
+    'url': 'https://www.idealo.de/preisvergleich/ProductCategory/26831.html?q=%s'
 }, {
-	'country': 'de',
-	'name': 'Alternate',
-	'url': 'https://www.alternate.de/html/search.html?query=%s'
+    'country': 'de',
+    'name': 'Alternate',
+    'url': 'https://www.alternate.de/html/search.html?query=%s'
 }, {
-	'country': 'fr',
-	'name': 'Amazon France',
-	'url': 'https://www.amazon.fr/s/field-keywords=%s'
+    'country': 'fr',
+    'name': 'Amazon France',
+    'url': 'https://www.amazon.fr/s/field-keywords=%s'
 }, {
-	'country': 'fr',
-	'name': 'Idealo France',
-	'url': 'https://www.idealo.fr/cat/26831/ram-ddr4.html?q=%s'
+    'country': 'fr',
+    'name': 'Idealo France',
+    'url': 'https://www.idealo.fr/cat/26831/ram-ddr4.html?q=%s'
 }, {
-	'country': 'it',
-	'name': 'Amazon Italy',
-	'url': 'https://www.amazon.it/s/field-keywords=%s'
+    'country': 'it',
+    'name': 'Amazon Italy',
+    'url': 'https://www.amazon.it/s/field-keywords=%s'
 }, {
-	'country': 'uk',
-	'name': 'Amazon UK',
-	'url': 'https://www.amazon.co.uk/s/field-keywords=%s'
+    'country': 'uk',
+    'name': 'Amazon UK',
+    'url': 'https://www.amazon.co.uk/s/field-keywords=%s'
 }, {
-	'country': 'uk',
-	'name': 'Overclockers UK',
-	'url': 'https://www.overclockers.co.uk/search?sSearch=%s'
+    'country': 'uk',
+    'name': 'Overclockers UK',
+    'url': 'https://www.overclockers.co.uk/search?sSearch=%s'
 }];
 
 window.store = {sites: sites, products: products, skus: skus};
